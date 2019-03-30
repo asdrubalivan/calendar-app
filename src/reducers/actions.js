9107e3bb0ee11713806b1005d48e54c49be9f677
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-03-30 15:39:56 -0400

    Added actions

---
+++ src/reducers/actions.js
@@ -0,0 +1,5 @@
+export const INIT = 'INIT';
+export const ADD_REMINDER = 'ADD_REMINDER';
+export const EDIT_REMINDER = 'EDIT_REMINDER';
+export const NEXT_MONTH = 'NEXT_MONTH';
+export const PREV_MONTH = 'PREV_MONTH';
