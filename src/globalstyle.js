3921456137a006feff0f3a3780d95295afacbbfa
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-03-30 15:20:34 -0400

    Added fonts to global style

--- src/globalstyle.js
+++ src/globalstyle.js
@@ -8,6 +8,11 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     > div {
       overflow-x: hidden;
     }
