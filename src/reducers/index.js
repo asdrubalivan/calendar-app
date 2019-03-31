1c4a4f1ade9520c125a91272ec645a7173ca4388
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-03-31 10:15:27 -0400

    Combining reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import CalendarReducer from './calendar'
+import ReminderReducer from './reminder'
 
-export default combineReducers({});
+export default combineReducers({
+  reminders: ReminderReducer,
+  calendar: CalendarReducer,
+});
