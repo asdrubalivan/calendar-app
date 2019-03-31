34d194f2143687dc3d4e10397f1ec21ade9196ad
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-03-31 13:14:54 -0400

    Finished processing reminders

--- src/selectors/calendar.js
+++ src/selectors/calendar.js
@@ -13,28 +13,51 @@ export const currentMonthSelector = createSelector(
 );
 
 const isWeekendFn = d => {
-  const days = moment(d).get('days');
+  const days = d.get('days');
   return days === 0 || days === 6;
 }
 
 const getProcessedReminders = (reminders, days, calendarDate) => {
+  if (calendarDate === null) {
+    return [];
+  }
   const momentCalendarDate = moment(calendarDate);
   const calendarDateMonth = momentCalendarDate.get('months');
   const formattedDate = momentCalendarDate.format('YYYY-MM-DD');
-  const isCurrentMonthFn = d => moment(d).get('months') === calendarDateMonth;
-  return days.map(d => {
-    const isCurrentMonth = isCurrentMonthFn(d);
-    const isWeekend = isWeekendFn(d);
+  const isCurrentMonthFn = d => d.get('months') === calendarDateMonth;
+  const mappedDays = days.map(d => {
+    const momentDay = moment(d);
+    const isCurrentMonth = isCurrentMonthFn(momentDay);
+    const isWeekend = isWeekendFn(momentDay);
     const remindersForDay = isCurrentMonth ?
-      reminders.filter(r => moment(r.date).format('YYYY-MM-DD') === formattedDate)
+      reminders
+        .filter(r => moment(r.date).format('YYYY-MM-DD') === formattedDate)
+        .sort((r1, r2) => moment(r1.date) >= moment(r2.date) ? 1 : -1)
       : [];
     return {
       isCurrentMonth,
       isWeekend,
       date: d,
+      week: momentDay.get('weeks'),
       reminders: remindersForDay,
+      formattedDay: momentDay.format('D'),
     };
   });
+  const retVal = [];
+  const weekCount = [];
+  let index = -1;
+  mappedDays.forEach(d => {
+    if (!weekCount.some(w => w === d.week)) {
+      index++;
+      weekCount.push(d.week);
+      retVal.push({
+        week: d.week,
+        days: []
+      });
+    }
+    retVal[index].days.push(d);
+  });
+  return retVal;
 };
 
 export const processedRemindersSelector = createSelector(
