0b9a15647c02272e0229f3d4f3432d6ec7e1b090
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-03-31 19:31:08 -0400

    Added markup for reminder form

--- src/addreminderform.js
+++ src/addreminderform.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import styled from 'styled-components';
+import { addReminder } from './reducers/actions'
+import { Datepicker } from 'react-formik-ui';
+import { utc as moment } from 'moment';
 
 const validationSchema = Yup.object().shape({
   color: Yup
@@ -22,9 +26,15 @@ const ColorDiv = styled.div`
   display: inline-block;
 `
 
+const FORMAT_DATE_PICKER = 'yyyy-MM-dd p';
+
 const getColor = (color, errors) => !errors ? color : 'white';
 
 class AddReminderForm extends Component {
+  onSubmit = (values) => {
+    this.props.onSubmit(values);
+    this.props.history.push('/');
+  }
   render() {
     const initialValues = {
       reminder: '',
@@ -33,24 +43,45 @@ class AddReminderForm extends Component {
     return (
       <Formik
         validationSchema={validationSchema}
-        initialValues={initialValues}>
-        {({ errors, touched, values }) => (
-          <Form>
-            <label htmlFor="reminder">Reminder</label>
-            <Field name="reminder" />
-            {errors.reminder && touched.reminder && <p>{errors.reminder}</p>}
-            <label htmlFor="color">Color</label>
-            <Field name="color" />
-            <ColorDiv style={{ backgroundColor: getColor(values.color, errors.color) }} />
-            {errors.color && touched.color && <p>{errors.color}</p>}
-            <label htmlFor="date">Date</label>
-            <Field name="date" />
-            {errors.date && touched.date && <p>{errors.date}</p>}
-          </Form>
-        )}
+        initialValues={initialValues}
+        onSubmit={this.onSubmit}>
+        {({ errors,
+          touched,
+          values,
+          isSubmitting,
+          isValid,
+        }) => (
+            <Form>
+              <label htmlFor="reminder">Reminder</label>
+              <Field name="reminder" />
+              {errors.reminder && touched.reminder && <p>{errors.reminder}</p>}
+              <label htmlFor="color">Color</label>
+              <Field name="color" />
+              <ColorDiv style={{ backgroundColor: getColor(values.color, errors.color) }} />
+              {errors.color && touched.color && <p>{errors.color}</p>}
+              <label htmlFor="date">Date</label>
+              <Datepicker
+                showTimeSelect
+                dateFormat={FORMAT_DATE_PICKER}
+                required={true}
+                onChange={(ev) => console.log('change', ev)}
+                name="date" />
+              {errors.date && touched.date && <p>{errors.date}</p>}
+              <button disabled={isSubmitting || !isValid} type="submit">Create reminder</button>
+            </Form>
+          )}
       </Formik>
     );
   }
 }
 
-export default AddReminderForm;
+const mapDispatchToProps = dispatch => ({
+  onSubmit: (values) => {
+    const m = moment(values.date);
+    m.subtract(values.date.getTimezoneOffset(), 'minutes')
+    values.date = m.toISOString();
+    dispatch(addReminder(values))
+  },
+});
+
+export default connect(null, mapDispatchToProps)(AddReminderForm);
