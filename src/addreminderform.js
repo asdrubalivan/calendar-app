2d2c4df7e4a17103eef29ab69f641dffd5823e1b
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-04-01 07:44:54 -0400

    Added utils for reminderform

--- src/addreminderform.js
+++ src/addreminderform.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Formik } from 'formik';
-import * as Yup from 'yup';
 import { addReminder } from './reducers/actions'
 import { utc as moment } from 'moment';
-import styled from 'styled-components';
 import ReminderForm from './reminderform';
-
-const validationSchema = Yup.object().shape({
-  color: Yup
-    .string()
-    .default('#000000')
-    .trim()
-    .lowercase()
-    .matches(/^#([a-f0-9]{3}|[a-f0-9]{6})$/, 'Color should match an hexadecimal number [#000, #fafafa]'),
-  reminder: Yup
-    .string()
-    .required('A reminder is required')
-    .max(30, 'No more than 30 characters are allowed'),
-  date: Yup.date().required('A date is required'),
-});
-
-const Title = styled.h2`
-  font-size: 1.8rem;
-  text-align: center;
-  margin: 1.8rem;
-`
+import { validationSchema, Title } from './utils/reminderform';
 
 class AddReminderForm extends Component {
   onSubmit = (values) => {
